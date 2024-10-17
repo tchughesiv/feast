da501ec20f3804b276534a08f596cb968aae1f8a
Tommy Hughes <tohughes@redhat.com>
2024-10-17 12:41:16 -0500

    dev
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- infra/feast-operator/internal/controller/services/registry.go
+++ infra/feast-operator/internal/controller/services/registry.go
@@ -186,8 +186,7 @@ func (feast *FeastServices) setClientConfigMap(cm *corev1.ConfigMap) error {
 	if err != nil {
 		return err
 	}
-	cm.Data = map[string]string{"string-feature_store.yaml": string(clientYaml)}
-	cm.BinaryData = map[string][]byte{"feature_store.yaml": clientYaml}
+	cm.Data = map[string]string{"feature_store.yaml": string(clientYaml)}
 	feast.FeatureStore.Status.ClientConfigMap = cm.Name
 	return nil
 }
