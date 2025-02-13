a28d7e6ff80f46ad4095e211cd2768c3e652c17d
Tommy Hughes <tohughes@redhat.com>
2025-02-13 17:47:40 -0600

    feast job
    Signed-off-by: Tommy Hughes <tohughes@redhat.com>

--- infra/feast-operator/api/v1alpha1/featurestore_types.go
+++ infra/feast-operator/api/v1alpha1/featurestore_types.go
@@ -69,10 +69,21 @@ type FeatureStoreSpec struct {
 	// +kubebuilder:validation:Pattern="^[A-Za-z0-9][A-Za-z0-9_]*$"
 	// FeastProject is the Feast project id. This can be any alphanumeric string with underscores, but it cannot start with an underscore. Required.
 	FeastProject string                `json:"feastProject"`
-	FeastJob     *batchv1.CronJobSpec  `json:"feastJob,omitempty"`
+	FeastJob     *JobConfig            `json:"feastJob,omitempty"`
 	Services     *FeatureStoreServices `json:"services,omitempty"`
 	AuthzConfig  *AuthzConfig          `json:"authz,omitempty"`
 	// FeastRepo    git.CloneOptions      `json:"feastRepo,omitempty"`
+}
+
+type JobConfig struct {
+	// to delete
+	Tmp *batchv1.CronJobSpec `json:"tmp,omitempty"`
+
+	// to keep
+
+	// The schedule in Cron format, see https://en.wikipedia.org/wiki/Cron.
+	Schedule         string `json:"schedule" protobuf:"bytes,1,opt,name=schedule"`
+	ContainerConfigs `json:",inline"`
 }
 
 // FeatureStoreServices defines the desired feast services. An ephemeral onlineStore feature server is deployed by default.
